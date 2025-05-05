a4c136c8565b2f3de46281342dfae2a651254ca7
italomtlindo <italomozersousa@gmail.com>
2025-05-05 13:59:55 -0300

    Add files via upload
    declarei por interio a variavel numero, depois pedi ao usiuario q escrevesse um numero e armazenei na variavel numero, depois coloquei se (if) se o numero descrito for 2==0 o numero é par senao (else)

---
+++ lista de exercicio 1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	int numeros;
+    
+    printf("digite um numero: \n");
+    scanf("%d",&numeros);
+    
+if (numeros % 2 ==0) {
+	
+	printf("o numero descrito é par: \n");
+	
+	}else {
+	
+	
+	printf("o numero é impar ");
+	
+	}
+	
+	return 0;
+}
